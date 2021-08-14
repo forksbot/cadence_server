b532cb7d7aa30a8092e8ec50917b30bd5a019332
Peter Donovan <peter@darkfusion.io>
2021-08-14 19:31:34 -0400

    Add Actix Web runtime and health-check endpoint Added the Actix Web framework to the project and configured a basic bootstrap / initialization process.
    The first endpoint, a "health check", was added to the API. This
    endpoint is extremely important and serves two _main_ purposes:
      - Sending a GET request to the '/health' endpoint tells us whether or
        not our service is up, available and healthy.
      - This endpoint allows for integration with other in-house services or
        third-party services (such as Kubernetes). When the '/health' check
        fails, we can be notified and/or automatically trigger a restart.
    
    __NOTE__: The project is currently using CircleCI v2.1 for continuous
    integration (instead of v2.0). The build is currently failing due to a
    misconfiguration related to the filesystem inside of the Docker
    container used.
      - This issue needs to be investigated and fixed __ASAP__
      - If unable to resolve the issue, falling back to using v2.0 and/or a
        different Dockerfile may be required.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 #[macro_use]
 extern crate log;
 
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
 mod logger;
 mod models;
 
 // Convenience type for error trait object
 type AppError = Box<dyn std::error::Error>;
 
-fn main() -> Result<(), AppError> {
+#[actix_web::main]
+async fn main() -> Result<(), AppError> {
     // Load environment variables from '.env'
     dotenv::dotenv()?;
     // Initialize custom logger
     logger::setup_logger("LOG_LEVEL", "LOG_FILE")?;
 
+    let bind_addr = env::var("BIND_ADDR").expect("'BIND_ADDR' must be set.");
+
+    info!("Bootstrapping server");
+
+    HttpServer::new(|| App::new().route("/health", web::get().to(health)))
+        .bind(&bind_addr)?
+        .run()
+        .await?;
+
     Ok(())
+}
+
+pub async fn health() -> impl Responder {
+    HttpResponse::Ok().body("healthy")
 }
